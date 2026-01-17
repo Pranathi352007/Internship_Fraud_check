f2dcb1e2fc58b8f260355790199e49fb47934d57
Pranathi352007 <pranathivadlapudi5@gmail.com>
2026-01-17 12:31:08 +0530

    Add files via upload

---
+++ browser-extension/popup.js
@@ -0,0 +1,11 @@
+{
+  "manifest_version": 3,
+  "name": "Internship Scam Checker",
+  "description": "Checks internship and job links for scam risk",
+  "version": "1.0",
+  "permissions": ["tabs", "activeTab"],
+  "host_permissions": ["<all_urls>"],
+  "action": {
+    "default_popup": "popup.html"
+  }
+}
